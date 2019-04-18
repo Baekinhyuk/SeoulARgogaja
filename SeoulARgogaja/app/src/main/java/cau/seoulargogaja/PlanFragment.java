dab7aea462726ba2a1b99efd4644f4e10e8d60c8
Baekinhyuk <bih94@hanmail.net>
2019-04-18 18:15:14 +0900

    UI 수정 및 달력 일자계산과정추가(일자에따른결과는 추후 예정)

--- SeoulARgogaja/app/src/main/java/cau/seoulargogaja/PlanFragment.java
+++ SeoulARgogaja/app/src/main/java/cau/seoulargogaja/PlanFragment.java
@@ -1,8 +1,10 @@
 package cau.seoulargogaja;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -22,11 +24,13 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -37,7 +41,7 @@ import cau.seoulargogaja.data.SpotDAO;
 import cau.seoulargogaja.data.SpotDTO;
 import cau.seoulargogaja.data.SpotParser;
 
-import static android.app.AlertDialog.THEME_HOLO_LIGHT;
+import static android.app.AlertDialog.THEME_DEVICE_DEFAULT_LIGHT;
 
 public class PlanFragment extends Fragment {
 
@@ -45,26 +49,22 @@ public class PlanFragment extends Fragment {
     private Animation fab_open, fab_close;
     private Boolean isFabOpen = false;
     private FloatingActionButton fab, fab1, fab2;
-    private EditText editTitle;
+    private TextView editTitle;
     private TextView startDate,endDate;
     private ImageView startImage,endImage,addImage;
     // datePicker 사용
     private SimpleDateFormat dateFormatter;
     private DatePickerDialog dialog,dialog2;
 
+
+    private Date sDate,eDate;
+    private int Date_diff_all;
+    private int Date_diff_month;
+    private int Date_diff_date;
+
     PlanDAO dao;
     InputMethodManager imm;
     ArrayList<PlanDTO> list;
-    //TEST
-    /*
-    List<PlanDTO> list = Arrays.asList(
-            new PlanDTO(1,"3월24일",0),
-            new PlanDTO(2,"중앙대","3월24일",0,0, "중앙대학교",0,0),
-            new PlanDTO(3,"중앙대2","3월24일",0,0, "중앙대학교2",0,0),
-            new PlanDTO(4,"3월25일",0),
-            new PlanDTO(5,"중앙대","3월25일",0,0, "중앙대학교",0,0),
-            new PlanDTO(6,"중앙대2","3월25일",0,0, "중앙대학교2",0,0)
-    );*/
 
     @Nullable
     @Override
@@ -175,21 +175,64 @@ public class PlanFragment extends Fragment {
         });
 
 
-        editTitle = (EditText) rootView.findViewById(R.id.plan_title);
+        editTitle = (TextView) rootView.findViewById(R.id.plan_title);
+
+        editTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder ad = new AlertDialog.Builder(getActivity());
+
+                ad.setTitle("제목을 입력해 주세요");       // 제목 설정
+
+                // EditText 삽입하기
+                final EditText et = new EditText(getActivity());
+                et.setText(editTitle.getText());
+                ad.setView(et);
+
+                // 확인 버튼 설정
+                ad.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String value = et.getText().toString();
+                        editTitle.setText(value);
+                        dialog.dismiss();     //닫기
+                        // Event
+                    }
+                });
+                // 취소 버튼 설정
+                ad.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();     //닫기
+                        // Event
+                    }
+                });
+
+                // 창 띄우기
+                ad.show();
+            }
+        });
+
+
+
+
 
         // 달력모양 입력 시 입력되는 형태
         startImage = (ImageView) rootView.findViewById(R.id.btn_calender);
         startDate = (TextView) rootView.findViewById(R.id.txt_calender);
         //startDate.setInputType(InputType.TYPE_NULL);
-        dateFormatter = new SimpleDateFormat("MM-dd",Locale.KOREA);
+        dateFormatter = new SimpleDateFormat("yyyy-MM-dd",Locale.KOREA);
         // 달력모양 눌렀을 때 datePicker 띄우기
         Calendar newCalendar = Calendar.getInstance();
-        dialog = new DatePickerDialog(getActivity(), THEME_HOLO_LIGHT, new DatePickerDialog.OnDateSetListener() {
+        dialog = new DatePickerDialog(getActivity(), THEME_DEVICE_DEFAULT_LIGHT, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                 Calendar newDate = Calendar.getInstance();
                 newDate.set(year, monthOfYear, dayOfMonth);
-                startDate.setText(dateFormatter.format(newDate.getTime()));
+                sDate = newDate.getTime();
+                if(check_Date_diff(sDate,eDate)){
+                    startDate.setText(dateFormatter.format(sDate));
+                }
             }
         }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
         startImage.setOnClickListener(new View.OnClickListener() {
@@ -198,19 +241,22 @@ public class PlanFragment extends Fragment {
                 dialog.show();
             }
         });
+
         // 달력모양 입력 시 입력되는 형태
         endImage = (ImageView) rootView.findViewById(R.id.btn_calender2);
         endDate = (TextView) rootView.findViewById(R.id.txt_calender2);
         //endDate.setInputType(InputType.TYPE_NULL);
         // 달력모양 눌렀을 때 datePicker 띄우기
         Calendar newCalendar2 = Calendar.getInstance();
-        dialog2 = new DatePickerDialog(getActivity(), THEME_HOLO_LIGHT, new DatePickerDialog.OnDateSetListener() {
+        dialog2 = new DatePickerDialog(getActivity(), THEME_DEVICE_DEFAULT_LIGHT, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                 Calendar newDate2 = Calendar.getInstance();
                 newDate2.set(year, monthOfYear, dayOfMonth);
-
-                endDate.setText(dateFormatter.format(newDate2.getTime()));
+                eDate = newDate2.getTime();
+                if(check_Date_diff(sDate,eDate)){
+                    endDate.setText(dateFormatter.format(eDate));
+                }
             }
         }, newCalendar2.get(Calendar.YEAR), newCalendar2.get(Calendar.MONTH), newCalendar2.get(Calendar.DAY_OF_MONTH));
         endImage.setOnClickListener(new View.OnClickListener() {
@@ -222,7 +268,64 @@ public class PlanFragment extends Fragment {
 
 
 
+
         return rootView;
+    }
+
+    public boolean check_Date_diff(Date sDate,Date eDate){
+        if(sDate != null && eDate != null){
+            if(eDate.compareTo(sDate) < 0){
+                Toast.makeText(getActivity(), "날짜가 안 맞습니다. 날짜를 확인해 주세요", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), Integer.toString(eDate.compareTo(sDate)), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            Date_diff_all = Date_diff(sDate,eDate);
+            Date_diff(sDate,Date_diff_all);
+
+            return true;
+        }
+        return true;
+    }
+
+    public int Date_diff(Date sDate,Date eDate){
+        try{
+            //SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+            long calDate = sDate.getTime() - eDate.getTime();
+
+            // Date.getTime() 은 해당날짜를 기준으로1970년 00:00:00 부터 몇 초가 흘렀는지를 반환해준다.
+            // 이제 24*60*60*1000(각 시간값에 따른 차이점) 을 나눠주면 일수가 나온다.
+            long calDateDays = calDate / ( 24*60*60*1000);
+
+            calDateDays = Math.abs(calDateDays);
+            Toast.makeText(getActivity(), "두 날짜의 날짜 차이:"+(int)calDateDays, Toast.LENGTH_SHORT).show();
+            return (int)calDateDays;
+        }
+        catch(Exception e)
+        {
+            return -1;
+            // 예외 처리
+        }
+    }
+
+    public void Date_diff(Date sDate,int Date_diff_all){
+        try{
+            String diff_M;
+            SimpleDateFormat dateFormat_mm = new SimpleDateFormat("MM", java.util.Locale.getDefault());
+            diff_M = dateFormat_mm.format(sDate);
+            Date_diff_month = Integer.parseInt(diff_M);
+            Toast.makeText(getActivity(), "첫달의 월 : "+Date_diff_month, Toast.LENGTH_SHORT).show();
+
+            String diff_d;
+            SimpleDateFormat dateFormat_dd = new SimpleDateFormat("dd", java.util.Locale.getDefault());
+            diff_d = dateFormat_dd.format(sDate);
+            Date_diff_date = Integer.parseInt(diff_d);
+            Toast.makeText(getActivity(), "첫달의 일 : "+Date_diff_date, Toast.LENGTH_SHORT).show();
+
+        }
+        catch(Exception e)
+        {
+            // 예외 처리
+        }
     }
 
     public void anim() {
